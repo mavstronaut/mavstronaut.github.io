d843f83dae1b0629350a891c43ff3921d5bcb8b6
mavstronaut <mavbarona@gmail.com>
2018-12-08 09:17:01 -0500

    additional fixes to index, blog and header

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Roclin.", "Data Scientists.", "Behavioural Economists.", "Developers.", "Freelancers.", "Researchers.", "Writers.", "Designers." "Collective Lineage." ],
       typeSpeed: 100,
       loop: true,
     });
